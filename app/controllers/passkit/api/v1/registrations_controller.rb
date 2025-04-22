e9a017a08604bc8a315ad6c0d9d082e8773b337e
Mandy Jor <mandy@txidigital.com>
2025-04-22 02:35:08 -0500

    store push service url

--- app/controllers/passkit/api/v1/registrations_controller.rb
+++ app/controllers/passkit/api/v1/registrations_controller.rb
@@ -90,7 +90,7 @@ module Passkit
           puts '--------------push_service_url',push_service_url
           puts '--------------params',params.inspect
 
-          device = Passkit::Device.find_or_create_by!(identifier: params[:device_id]).update(push_token: push_token, push_service_url: push_service_url)
+          device = Passkit::Device.find_or_create_by!(identifier: params[:device_id])
           device.update(push_token: push_token, push_service_url: push_service_url)
           puts '--------------device',device.inspect 
           
